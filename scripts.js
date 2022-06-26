97e6be21ca92cf480e831cd6d9ee0bf6e7594194
apinto5 <54562918+apinto5@users.noreply.github.com>
2022-06-26 14:04:56 -0400

    remove alert

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
 
 var html = document.querySelector('html');
-alert("KIND OF RUNNING");
-
   var keywordSearch = document.getElementById('hiddenBar');
   keywordSearch.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
